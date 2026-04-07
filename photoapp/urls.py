f4902625a2ef9a0b02f53b0cf2ee44d528c7064c
Demanlal Sahu <demansahu335@gmail.com>
2026-04-07 22:43:42 +0530

    add test home route

--- photoapp/urls.py
+++ photoapp/urls.py
@@ -15,29 +15,40 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.contrib import admin
+# from album.views import register
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('',include('album.urls')),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+#     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='loginpass/reset_pass.html'),name='password_reset'),
+#     path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='loginpass/password_reset_done.html'),
+#          name='password_reset_done'),
+#
+#     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='loginpass/password_reset_confirm.html'),
+#          name='password_reset_confirm'),
+#
+#     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='loginpass/password_reset_complete.html'),
+#          name='password_reset_complete'),
+#     path('register/', register, name='register'),
+#
+# ]
+#
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 from django.contrib import admin
-from album.views import register
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Working 🚀")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-    path('',include('album.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='loginpass/reset_pass.html'),name='password_reset'),
-    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='loginpass/password_reset_done.html'),
-         name='password_reset_done'),
-
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='loginpass/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='loginpass/password_reset_complete.html'),
-         name='password_reset_complete'),
-    path('register/', register, name='register'),
-
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
